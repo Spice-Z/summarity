f40e389997c366720d1861016857049cb6ff2710
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2024-02-13 22:51:27 -0800

    update tauri, add stylex

--- app/routes/setting.tsx
+++ app/routes/setting.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import * as stylex from '@stylexjs/stylex';
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,9 +9,17 @@ export const meta: MetaFunction = () => {
 };
 export default function Setting() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div {...stylex.props(styles.container)}>
       <h1>Welcome to Setting</h1>
       <Link to="/">index</Link>
     </div>
   );
 }
+
+const styles = stylex.create({
+  container: {
+    fontFamily: "system-ui, sans-serif",
+    lineHeight: 1.8,
+    color:'red'
+  }
+});
